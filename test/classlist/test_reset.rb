b5d4fd01327f67e42e491eb579095f3c05f237ac
Jakob Skjerning <jakob@mentalized.net>
2022-11-04 15:45:30 +0100

    Add Classlist::Reset to replace all entries
    I considered using Classlist::Replace as the name, however given that we
    have a `Classlist#replace` method that replaces a single token with
    another, I worried there'd be confusion when Classlist::Replace did
    something entirely different.

---
+++ test/classlist/test_reset.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+require "classlist/reset"
+
+class TestClasslistReset < Minitest::Test
+  def test_inherits_from_classlist
+    classlist = Classlist::Reset.new("foo")
+    assert_kind_of(Classlist, classlist)
+  end
+end
+
+class TestAddingToClasslist < Minitest::Test
+  def test_replaces_existing_tokens_in_the_classlist
+    classlist = Classlist.new("foo bar baz")
+    reset = Classlist::Reset.new("baz foo")
+
+    result = classlist + reset
+
+    assert_equal(Classlist.new("baz foo"), result)
+  end
+
+  def test_replaces_an_empty_classlist
+    classlist = Classlist.new("")
+    reset = Classlist::Reset.new("something")
+
+    result = classlist + reset
+
+    assert_equal(Classlist.new("something"), result)
+  end
+end
