4a390b7aeaaf1c5b10ed972cd189a47153acb94e
Jakob Skjerning <jakob@mentalized.net>
2022-11-06 15:50:34 +0100

    Verify that operations can be added via +

--- test/classlist/test_operation.rb
+++ test/classlist/test_operation.rb
@@ -6,10 +6,22 @@ require "classlist/add"
 require "classlist/remove"
 
 class TestClasslistOperation < Minitest::Test
+  def test_add
+    base = Classlist.new("foo")
+    result = base + Classlist::Add.new("bar")
+    assert_equal(["foo", "bar"], result.to_a)
+  end
+
   def test_add_as_manual_operations
     base = Classlist.new("foo")
     base.add_operation(Classlist::Add.new("bar"))
     assert_equal(["foo", "bar"], base.to_a)
+  end
+
+  def test_remove
+    base = Classlist.new("this that")
+    result = base + Classlist::Remove.new("this")
+    assert_equal(["that"], result.to_a)
   end
 
   def test_remove_as_manual_operations
@@ -18,10 +30,23 @@ class TestClasslistOperation < Minitest::Test
     assert_equal(["that"], base.to_a)
   end
 
+  def test_reset
+    base = Classlist.new("this that")
+    result = base + Classlist::Reset.new("something else")
+    assert_equal(["something", "else"], result.to_a)
+  end
+
   def test_reset_as_manual_operations
     base = Classlist.new("this that")
     base.add_operation(Classlist::Reset.new("something else"))
     assert_equal(["something", "else"], base.to_a)
+  end
+
+  def test_remove_then_add
+    result = Classlist.new("notthis")
+    result += Classlist::Remove.new("notthis")
+    result += Classlist::Add.new("this")
+    assert_equal(["this"], result.to_a)
   end
 
   def test_remove_then_add_as_manual_operations
@@ -29,6 +54,13 @@ class TestClasslistOperation < Minitest::Test
     base.add_operation(Classlist::Remove.new("notthis"))
     base.add_operation(Classlist::Add.new("this"))
     assert_equal(["this"], base.to_a)
+  end
+
+  def test_add_then_remove
+    result = Classlist.new("foo bar")
+    result += Classlist::Add.new("foo bar")
+    result += Classlist::Remove.new("not this")
+    assert_equal(["foo", "bar"], result.to_a)
   end
 
   def test_add_then_remove_as_manual_operations
